c544a046e4ea089b2a370b35f3dd420d188f6c7f
fduch <alex.medwedew@gmail.com>
2014-12-02 14:41:47 +0300

    add dumpers to dump several types of thrift definition files

---
+++ src/Exception/DumpException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Global Trading Technologies Ltd.
+ *
+ * The following source code is PROPRIETARY AND CONFIDENTIAL. Use of
+ * this source code is governed by the Global Trading Technologies Ltd. Non-Disclosure
+ * Agreement previously entered between you and Global Trading Technologies Ltd.
+ *
+ * By accessing, using, copying, modifying or distributing this
+ * software, you acknowledge that you have been informed of your
+ * obligations under the Agreement and agree to abide by those obligations.
+ *
+ * Author: Alex Medvedev
+ * Date: 11/28/14
+ */
+
+namespace Gtt\ThriftGenerator\Exception;
+
+use \RuntimeException;
+
+/**
+ * Exception for definition's file dump problems
+ *
+ * @author Alex Medvedev
+ */
+class DumpException extends \RuntimeException
+{
+
+}
