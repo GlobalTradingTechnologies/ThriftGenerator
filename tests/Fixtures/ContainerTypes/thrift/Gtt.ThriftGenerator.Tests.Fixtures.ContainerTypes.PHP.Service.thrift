d005a2e903b19570363bb618be832a48a4fa195c
fduch <alex.medwedew@gmail.com>
2014-12-18 18:52:29 +0300

    cast PHP array to thrift maps (instead of lists)  by default

--- tests/Fixtures/ContainerTypes/thrift/Gtt.ThriftGenerator.Tests.Fixtures.ContainerTypes.PHP.Service.thrift
+++ tests/Fixtures/ContainerTypes/thrift/Gtt.ThriftGenerator.Tests.Fixtures.ContainerTypes.PHP.Service.thrift
@@ -4,6 +4,6 @@ namespace php Gtt.ThriftGenerator.Tests.Fixtures.ContainerTypes.PHP
 
 service Service {
     list<Gtt.ThriftGenerator.Tests.Fixtures.ContainerTypes.PHP.DTO.DTO1> receivesListOfDTOsAndReturnsListOfDTOs(1:list<Gtt.ThriftGenerator.Tests.Fixtures.ContainerTypes.PHP.DTO.DTO1> listOfDTOs),
-    list<string> receiveArrayReturnsArrayWithNativeAnnotations(1:list<string> array),
+    map<string, string> receiveArrayReturnsArrayWithNativeAnnotations(1:map<string, string> array),
     list<i32> receiveArrayReturnsArrayWithExplicitAnnotations(1:list<string> strings)
 }
